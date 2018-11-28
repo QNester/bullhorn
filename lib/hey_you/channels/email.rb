5ceaaca77e96184f5af1b3c0c177d31739f86d2e
Sergey Nesterov <qnesterr@gmail.com>
2018-11-28 22:37:41 +0300

    Custom mailer for email channel

--- lib/hey_you/channels/email.rb
+++ lib/hey_you/channels/email.rb
@@ -5,9 +5,32 @@ module HeyYou
   module Channels
     class Email < Base
       class << self
-        def send!(builder, to:)
+        def send!(builder, to:, **options)
           raise CredentialsNotExists unless credentials_present?
 
+          method = config.email.default_mailing ? :send_via_mail : :send_via_custom_class
+          public_send(method, builder, to, options)
+        end
+
+        # Send email via ActionMailer instance.
+        # User must set mailer_class and mailer_method in notification collection
+        def send_via_custom_class(builder, to, **options)
+          mailer = mailer_class_from_builder(builder, options)
+
+          mailer_method = options[:mailer_method] ||
+            builder.email.mailer_method ||
+            config.email.default_mailer_method
+
+          delivery_method = options[:delivery_method] ||
+            builder.email.delivery_method ||
+            config.email.default_delivery_method
+
+          mailer_msg = mailer.public_send(mailer_method, data: builder.email, to: to)
+          mailer_msg.public_send(delivery_method)
+        end
+
+        # Send email with standard mail (gem 'mail')
+        def send_via_mail(builder, to, **_)
           context = self
           mail = Mail.new do
             from HeyYou::Config.instance.email.from
@@ -22,17 +45,40 @@ module HeyYou
         end
 
         def get_body(body_text)
-          # TODO: Load layout here.
+          # TODO: Load layout from config here.
           body_text
         end
 
         def required_credentials
-          %i[delivery_method from]
+          %i[from]
         end
 
+        private
+
+        def mailer_class_from_builder(builder, **options)
+          mailer_class = options[:mailer_class] ||
+            builder.email.mailer_class ||
+            config.email.default_mailer_class
+          unless mailer_class
+            raise MailerClassNotDefined, 'You must set mailer_class in notifications collection or pass :mailer_class option'
+          end
+
+          begin
+            mailer = Object.const_get(mailer_class.to_s)
+          rescue NameError
+            raise ActionMailerClassNotDefined, "Mailer #{mailer_class} not initialized"
+          end
+
+          mailer
+        end
       end
 
-      class CredentialsNotExists < StandardError; end
+      class CredentialsNotExists < StandardError;
+      end
+      class ActionMailerClassNotDefined < StandardError;
+      end
+      class MailerClassNotDefined < StandardError;
+      end
     end
   end
 end
--- spec/lib/builder/email_spec.rb
+++ spec/lib/builder/email_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 RSpec.describe HeyYou::Builder::Email do
-  include_examples :have_readers, :subject, :body, :layout
+  include_examples :have_readers, :subject, :body, :layout, :mailer_class, :mailer_method
 end
